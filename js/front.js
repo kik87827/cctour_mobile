aba8be976a1701937c43933193ffa42af0aeb835
in kyung <kik87827@gmail.com>
2023-06-20 17:33:40 +0900

    고객사 수정 요청

--- js/front.js
+++ js/front.js
@@ -320,3 +320,20 @@ function mcGalleryPro(){
 		}
 	}
 }
+
+function mcMdpickPro(){
+	let data_container_slide = document.querySelectorAll(".mc_mdpick_container .swiper-slide");
+	let data_swiper_obj = null;
+	if(data_swiper_obj !== null){
+		data_swiper_obj.update();
+	}else{
+		if(data_container_slide.length>1){
+			data_swiper_obj = new Swiper(".mc_mdpick_container", {
+				speed : 1000,
+				pagination: {
+					el: '.mc_mdpick_container .mc-pagination',
+				}
+			});
+		}
+	}
+}
